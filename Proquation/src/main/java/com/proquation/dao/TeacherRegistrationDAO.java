f21c31afac072f1e07ef4c4b8f0f0f45d262c105
Rahul Suresh <rsures13@asu.edu>
2019-10-29 19:31:18 -0700

    Added Teacher Registration DAO

---
+++ Proquation/src/main/java/com/proquation/dao/TeacherRegistrationDAO.java
@@ -0,0 +1,35 @@
+package com.proquation.dao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class TeacherRegistrationDAO {
+	public void registerTeacher(String name, String username, String password, String grade){
+		String query = "insert into Teacher (teacher_fullname, teacher_username, teacher_password, teacher_grade) values (?,?,?,?)";
+		Connection connection = null;
+		PreparedStatement statement = null;
+		try {
+			connection = (Connection) DatabaseConnection.getDBConnectionInstance().getConnection();
+			statement = connection.prepareStatement(query);
+			statement.setString(1,name);
+			statement.setString(2,username);
+			statement.setString(3,password);
+			statement.setString(4,grade);
+			statement.execute();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if(statement != null)
+					statement.close();
+				if(connection != null)
+					connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+}
