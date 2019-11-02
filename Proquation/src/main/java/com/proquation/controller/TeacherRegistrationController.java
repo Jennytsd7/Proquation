150de85997151671b12c4a6167dd98676f2681a5
Rahul Suresh <rsures13@asu.edu>
2019-11-01 20:29:01 -0700

    Added the author name  to all the files have I have created

--- Proquation/src/main/java/com/proquation/controller/TeacherRegistrationController.java
+++ Proquation/src/main/java/com/proquation/controller/TeacherRegistrationController.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.proquation.dao.TeacherRegistrationDAO;
 import com.proquation.dao.TeacherUsernameSearch;
-
+//Author name: Rahul Suresh
 @WebServlet("/teacherregister")
 public class TeacherRegistrationController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
