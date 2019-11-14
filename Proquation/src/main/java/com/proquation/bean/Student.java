304154a5e91b4779b52dbdb07f1e5f5f00187e7e
Rahul Suresh <rsures13@asu.edu>
2019-11-14 10:44:21 -0700

    Have created the student bean and corrected the routing from the Teacher Login page

--- Proquation/src/main/java/com/proquation/bean/Student.java
+++ Proquation/src/main/java/com/proquation/bean/Student.java
@@ -1,17 +1,47 @@
 package com.proquation.bean;
+//Author Rahul
+public abstract class Student {
 
-/** @author Raghavan Sreenivasa
- *  version 1.0
- *  This is a Java bean class for student
- */
 
-public interface Student {
-    public String getStudentFullname();
-    public void setStudentFullname(String studentFullname);
-    public String getStudentUsername();
-    public void setStudentUsername(String studentUsername);
-    public String getStudentPassword();
-    public void setStudentPassword(String studentPassword);
-    public String getStudentGrade();
-    public void setStudentGrade(String studentGrade);
+	private int student_id;
+	private String student_name;
+	private String student_username;
+	private String student_grade;
+
+	public int getStudent_id() {
+		return student_id;
+	}
+	public void setStudent_id(int student_id) {
+		this.student_id = student_id;
+	}
+	public String getStudent_name() {
+		return student_name;
+	}
+	public void setStudent_name(String student_name) {
+		this.student_name = student_name;
+	}
+	public String getStudent_username() {
+		return student_username;
+	}
+	public void setStudent_username(String student_username) {
+		this.student_username = student_username;
+	}
+	public String getStudent_grade() {
+		return student_grade;
+	}
+	public void setStudent_grade(String student_grade) {
+		this.student_grade = student_grade;
+	}
+	
+	public abstract String getStudentFullname();
+	public abstract void setStudentFullname(String studentFullname);
+	public abstract String getStudentUsername();
+	public abstract void setStudentUsername(String studentUsername);
+	public abstract String getStudentPassword();
+	public abstract void setStudentPassword(String studentPassword);
+	public abstract String getStudentGrade();
+	public abstract void setStudentGrade(String studentGrade);
+
 }
+
+
