0561eedc3d2424013d0b6aefc5968d19a1bb3c94
Janani Anand <janand3@asu.edu>
2019-11-19 19:36:53 -0700

    US126-T133 Form json data from question

--- Proquation/src/main/webapp/js/quizQuestions.js
+++ Proquation/src/main/webapp/js/quizQuestions.js
@@ -1,36 +1,40 @@
 
 /**
  * @author Janani Anand 
- * version 1 
+ * version 1.2
  * This function stores the values entered for
  * each question field and stores the values as a json
  */
 
 function submitform(){
+	var questionsJson = sessionStorage.getItem('questions');
+	var questionsCount = sessionStorage.getItem('questionsCount');
+	if(questionsJson == null) {
+		questionsJson = {"Questions":[]};
+		questionsCount = 0;
+	}
 	const questionNumber = document.getElementById('questionNumber').value;
 	const question = document.getElementById('question').value;
 	const option1 = document.getElementById('option1').value;
 	const option2 = document.getElementById('option2').value;
 	const option3 = document.getElementById('option3').value;
 	const option4 = document.getElementById('option4').value;
-	const correctanswer = document.getElementById('correctanswer').value;
-	var questionsJson = {};
+	const correctanswer = document.getElementById('correctAns').value;
     if(questionNumber.length == 0 || question.length == 0 || option1.length == 0 || option2.length == 0 || option3.length == 0 || option3.length == 0 || option4.length == 0 || correctanswer.length == 0) {   	
+    	alert("Enter all the fields");
     }
     else {
-    	questionsJson = {
-    			"Questions" : [
-    			    			  {"QuestionNumber": questionNumber,
-    			    				"Question": question,
-    			    				"Option1": option1,
-    			    				"Option2": option2,
-    			    				"Option3": option3,
-    			    				"Option4": option4,
-    			    				"CorrectAnswer": correctanswer}
-    			    				]
-    			    	}
-    	
+    	let questionToAdd = [{"QuestionNumber": questionNumber,
+				"Question": question,
+				"Option1": option1,
+				"Option2": option2,
+				"Option3": option3,
+				"Option4": option4,
+				"CorrectAnswer": correctanswer
+				}];
+    	questionsJson["Questions"][questionsCount] = questionToAdd;
+    	sessionStorage.setItem('questions', questionsJson);
+    	sessionStorage.setItem('questionsCount', questionsCount);
     }
     console.log(questionsJson);
-    	
 }
