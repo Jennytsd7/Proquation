6e5a7856becab63d06ed526ad3e85344d0a0fb43
Rahul Suresh <rsures13@asu.edu>
2019-11-20 20:54:11 -0700

    US126-T141: Updated the Addquiz DAO to add the JSON to DB

--- Proquation/src/main/java/com/proquation/dao/AddQuizDAO.java
+++ Proquation/src/main/java/com/proquation/dao/AddQuizDAO.java
@@ -3,22 +3,23 @@ package com.proquation.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+//import org.json.simple.JSONObject;
+//import org.json.simple.parser.JSONParser;
 
 public class AddQuizDAO {
-	public void AddQuiz(String quizData, int teacherID, String quizGrade) {
-		String query = "insert into proquation.Quiz(quiz_data, teacher_id, quiz_grade) values(?,?,?)";
+	public void AddQuiz(String quizData, String teacherUsername, String quizGrade) {
+		String query = "insert into proquation.Quiz(quiz_data, quiz_grade, teacher_username) values(?,?,?)";
 		Connection connection = null;
 		PreparedStatement statement = null;
-		JSONObject quizDataJson = null;
-		JSONParser jsonparser = new JSONParser();
+//		JSONObject quizDataJson = null;
+//		JSONParser jsonparser = new JSONParser();
 		try {
 			connection = (Connection) DatabaseConnection.getDBConnectionInstance().getConnection();
-			quizDataJson = (JSONObject) jsonparser.parse(quizData);
+			//quizData="{\"Questions\": {\"0\": {\"QuestionNumber\": \"1\",\"Question\": \"what\",\"Option1\": \"a\",\"Option2\": \"b\",\"Option3\": \"c\",\"Option4\": \"d\",\"CorrectAnswer\": \"d\"},\"1\": {\"QuestionNumber\": \"2\",\"Question\": \"how\",\"Option1\": \"a\",\"Option2\": \"b\",\"Option3\": \"c\",\"Option4\": \"d\",\"CorrectAnswer\": \"c\"}}}";
+			//quizDataJson = (JSONObject) jsonparser.parse(quizData);
 			statement = connection.prepareStatement(query);
-			statement.setObject(1, quizDataJson);
-			statement.setInt(2, teacherID);
+			statement.setObject(1, quizData);
+			statement.setString(2, teacherUsername);
 			statement.setString(3, quizGrade);
 			statement.execute();
 		}
